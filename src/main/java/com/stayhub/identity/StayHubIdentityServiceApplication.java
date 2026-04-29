d1fb404357b3b28051a561ae2ea9451b03eb8793
Huy Nguyen <nguyengiahuy27201@gmail.com>
2026-04-29 14:18:04 +0700

    Initialize the Identity Service

---
+++ src/main/java/com/stayhub/identity/StayHubIdentityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stayhub.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHubIdentityServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StayHubIdentityServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/com/stayhub/identity/StayhubIdentityServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.stayhub.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayhubIdentityServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
